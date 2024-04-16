0f633fe63b797b05d9b7cd74e11df20d80691dba
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-04-16 18:43:27 +0300

    OnMac2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:RoutineCare/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("Routine_Database");
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
 import 'package:routine_care/pages/home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
@@ -16,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.pink),
     );
   }
-}
+}*/
 
 /*import 'package:flutter/material.dart';
 import 'package:routine_care/pages/home_page.dart';
